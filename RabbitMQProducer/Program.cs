e4fefe9223339010a4ca8f9577f717fe8a390cc3
Jorge Pérez González <jpgseva@hotmail.com>
2021-03-01 17:52:23 +0100

    Agregar archivos de proyecto.

---
+++ RabbitMQProducer/Program.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQProducer
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            bool salir = false;
+            var factory = new ConnectionFactory {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+            using (var connection = factory.CreateConnection()) {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("demo-queue",
+                        durable: true,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+                    
+                    while (!salir)
+                    {
+                        Console.WriteLine("Escribe un mensaje para enviar a cola:(S) Salir");
+                        string mensaje = Console.ReadLine().ToString();
+                        if (mensaje.ToLower().Equals("s"))
+                        {
+                            salir = true;
+                        }
+                        var message = new { mensaje };
+                        var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+                        channel.BasicPublish("", "demo-queue", null, body);
+                        
+                        
+                    }
+                    
+                }
+            }
+            
+        }
+    }
+}
